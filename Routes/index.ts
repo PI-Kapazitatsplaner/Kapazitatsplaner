d6b821d23824bf5d02ed7070037b4fcf789a1176
roehh <git@roeh.ch>
2022-01-24 16:08:15 +0100

    auto deploy

--- Routes/index.ts
+++ Routes/index.ts
@@ -6,7 +6,6 @@ const prisma = new PrismaClient()
 let router = express.Router();
 
 router.get('/', async (req, res) => {
-    await prisma.user.create({ data: { email: 'test' } })
     res.json(await prisma.user.findMany({}));
 });
 
