f7c1821742a2c26196d34bf87040e858ab226276
roehh <git@roeh.ch>
2022-02-07 10:51:51 +0100

    fix git

--- Prisma/seed.ts
+++ Prisma/seed.ts
@@ -3,17 +3,7 @@ const prisma = new PrismaClient()
 
 //Add data here to seed the database
 const main = async () => {
-    await prisma.user.deleteMany({})
-    await prisma.user.createMany({
-        data: [
-            {
-                email: 'd'
-            },
-            {
-                email: 'q'
-            }
-        ]     
-    })
+
 };
 
 if(process.env.seed?.toLowerCase() === 'true') {
