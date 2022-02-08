da2330ccec20ca4bcf3e9543fe34f76377d774f6
gifederspiel <gian.fedrespiel@bluewin.ch>
2022-02-08 09:06:12 +0100

    Route paths updated

--- Routes/user.ts
+++ Routes/user.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
+import path from "path";
+
 import ejs from 'ejs';
 
 let router = express.Router();
 
 router.get('/', async (req, res) => {
-    const header = await ejs.renderFile("C:/Users/Gian Federspiel/Documents/GitHub/Kapazitatsplaner/Views/header.ejs", {currSite:2});
+    const header = await ejs.renderFile(path.join(__dirname, '../Views/header.ejs'), {currSite:2});
     res.render("mein_kalender", {header:header});
 });
 
