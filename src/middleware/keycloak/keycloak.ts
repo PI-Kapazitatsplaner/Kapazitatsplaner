62b5dc0c77caca7b8a2ede2615afc9298ad91f9e
roehh <git@roeh.ch>
2022-02-09 08:50:05 +0100

    fix deployment

--- src/middleware/keycloak/keycloak.ts
+++ src/middleware/keycloak/keycloak.ts
@@ -3,7 +3,13 @@ import Keycloak from 'keycloak-connect';
 
 //Keycloak middleware
 export const memoryStore = new session.MemoryStore();
-let keycloak = new Keycloak({ store: memoryStore });
+let keycloak = new Keycloak({ store: memoryStore }, {
+    "realm": "staging",
+    "auth-server-url": "https://auth.wuersch.cloud/auth/",
+    "ssl-required": "external",
+    "resource": "suva-ppk",
+    "confidential-port": 0
+});
 
 //Mocking Keycloak for Testing
 if (process.env.NODE_ENV === 'test') {
