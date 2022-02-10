12375aeace1829a7815d91ba12d3fef860432205
gifederspiel <gian.fedrespiel@bluewin.ch>
2022-02-10 14:58:05 +0100

    Verwaltung erweitert (halbstatisch)

--- Public/controller.js
+++ Public/controller.js
@@ -2,18 +2,21 @@ var darkBg      = "#121212";
 var darkBg2     = "#1c1c1c";
 var darkText    = "#d3d3d3";
 var darkLine    = "#454545";
+var darkInput   = "#333333";
 
 var lightBg     = "#ffffff";
 var lightBg2    = "#f7f7f7";
 var lightText   = "#666666";
 var lightLine   = "#d3d3d3";
+var lightInput  = "#ffffff";
 
 function setColors(val){
 
-    var body = document.getElementById("body");
-    var text = document.getElementsByClassName("text");
+    var body    = document.getElementById("body");
+    var text    = document.getElementsByClassName("text");
     var content = document.getElementById("mainContent");
-    var line = document.getElementsByClassName("linecol");
+    var line    = document.getElementsByClassName("linecol");
+    var input   = document.getElementsByClassName("input");
 
     if (val == "dark"){
         //Hintergrundfarbe ändern
@@ -26,6 +29,10 @@ function setColors(val){
         for (let i = 0; i < line.length; i++) {
             //Linienfarbe ändern
             line.item(i).style.setProperty("--line", darkLine);
+        }
+        for (let i = 0; i < input.length; i++) {
+            //input ändern
+            input.item(i).style.setProperty("--input", darkInput);
         }
 
     }
@@ -40,6 +47,10 @@ function setColors(val){
         for (let i = 0; i < line.length; i++) {
             //Linienfarbe ändern
             line.item(i).style.setProperty("--line", lightLine);
+        }
+        for (let i = 0; i < line.length; i++) {
+            //Linienfarbe ändern
+            line.item(i).style.setProperty("--line", lightInput);
         }
     }
 }
--- Routes/sprint_settings.ts
+++ Routes/sprint_settings.ts
@@ -1,12 +1,15 @@
 import express from 'express';
-
+import path from 'path'
 import ejs from 'ejs';
 
 let router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.render("sprint_verwaltung", {query : req.query});
+router.get('/:year/:pi', async (req, res) => {
+    if (req.params.pi?.match(/^.*.js|.*.css$/)) {
+        res.sendFile(path.join(__dirname, '../Public', req.params.pi));
+    }
+    else{
+        res.render("sprint_verwaltung", {query : req.query, params : req.params});
+    }    
 });
-
-
 export = router;
