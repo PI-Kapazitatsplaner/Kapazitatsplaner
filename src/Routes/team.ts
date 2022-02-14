dc61fc0dfc8f78dd453e42f2d2fef33fb93f3654
roehh <git@roeh.ch>
2022-02-14 14:49:25 +0100

    fix tests

--- src/Routes/team.ts
+++ src/Routes/team.ts
@@ -3,7 +3,7 @@ import express from 'express';
 let router = express.Router();
 
 router.get('/', async (req, res) => {
-    const header = { currSite: 2, username: req.user.name };
+    const header = { currSite: 1, username: req.user.name };
     res.render("team_kalender", { header, prefersWhiteMode: req.user.prefersWhiteMode });
 });
 
