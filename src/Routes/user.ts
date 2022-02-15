828144a1d916a6f4e0a41f8fe6ec04276baf2b44
iccee0 <git@roeh.ch>
2022-02-15 16:25:25 +0100

    some small steps

--- src/Routes/user.ts
+++ src/Routes/user.ts
@@ -1,19 +1,65 @@
 import express from 'express';
+import path from 'path';
+import { param } from '.';
+import prisma from '../prisma/client';
 
 let router = express.Router();
 
-router.get('/', async (req, res) => {
-    const now = new Date();
-    const header = { currSite: 2, username: req.user.name };
-    const calendar = {
-        fillerDays: now.getDay(),
-        daysInMonth: new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate(),
-        activeDay: now.getDate(),
-        month: now.toLocaleString("en-US", { month: "long" }),
-        year: now.getFullYear(),
+router.get('/:year?/:month?', async (req, res) => {
+    const year = req.params.year;
+    const month = req.params.month;
+    if (year?.match(/^.*.js|.*.css$/)) {
+        res.sendFile(path.join(__dirname, '../Public', year));
+    } else if (month?.match(/^.*.js|.*.css$/)) {
+        res.sendFile(path.join(__dirname, '../Public', month));
+    } else {
+
+        if (validateParams(req.params)) {
+            const date = new Date(Number(year), Number(month) - 1, new Date().getDate());
+            const calendar = {
+                fillerDays: new Date(date.getFullYear(), date.getMonth(), 0).getDay(),
+                daysInMonth: new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(),
+                activeDay: !(Number(year) === new Date().getFullYear() && Number(month) === new Date().getMonth() + 1) ? 0 : date.getDate(),
+                monthString: date.toLocaleString("de-CH", { month: "long" }),
+                month: date.getMonth() + 1,
+                year: date.getFullYear(),
+            }
+            const header = { currSite: 2, username: req.user.name };
+            res.render("mein_kalender", { header, prefersWhiteMode: req.user.prefersWhiteMode, calendar });
+        }
+        else {
+            if (year !== undefined && Number(year) >= 2020 && Number(year) < 2100) {
+                res.redirect('/mein_kalender/' + year + "/" + 1)
+            } else {
+                res.redirect('/mein_kalender/' + new Date().getFullYear() + "/" + (new Date().getMonth() + 1))
+            }
+        }
     }
-    res.render("mein_kalender", { header, prefersWhiteMode: req.user.prefersWhiteMode, calendar });
 });
 
+router.post('/:year/:month', async (req, res) => {
+    if (validateParams(req.params)) {
+        await prisma.abwesenheit.create({
+            data: {
+                    user:{
+                        connect:{ sub: req.user.sub }
+                    },
+                    date: new Date(Number(req.params.year), Number(req.params.month), req.body.day)
+                }
+        });
+    } else {
+        res.send(400);
+    }
+});
+
+function validateParams(params: any): Boolean {
+    return (!(params.year === undefined || params.month === undefined)
+        && Number(params.year) >= 2020 && Number(params.year) < 2100
+        && Number(params.month) >= 1 && Number(params.month) <= 12)
+}
+
+function validateBody(body: any): Boolean {
+    return (body.day !== undefined && Number(body.day) >= 1 && Number(body.day) < 32)
+}
 
 export = router;
--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,23 +1,36 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import prisma from "./client";
 
 //Add data here to seed the database
 const main = async () => {
+    await prisma.abwesenheit.deleteMany({});
     await prisma.user.deleteMany({});
     await prisma.user.createMany({
         data: [
             {
                 sub: 'c2842822-67f5-4759-8db8-a431ddfc3500' //hr7 -- Default Test User
             },
-            {                
+            {
                 sub: '4296e3d8-a609-4ffa-b27a-3106ed7a5126', //gif
                 preferencesWhiteMode: true
             }
         ]
     });
+    await prisma.abwesenheit.createMany({
+        data: [
+            {
+                userSub: 'c2842822-67f5-4759-8db8-a431ddfc3500',
+                date: new Date(2022, 1, 3)
+            },
+            {
+                userSub: 'c2842822-67f5-4759-8db8-a431ddfc3500',
+                date: new Date(2022, 2, 5)
+            },
+        ]
+    });
+
 };
 
-if(process.env.seed?.toLowerCase() === 'true') {
+if (process.env.seed?.toLowerCase() === 'true') {
     main()
         .catch((error) => {
             console.error(error);
@@ -26,6 +39,6 @@ if(process.env.seed?.toLowerCase() === 'true') {
         .finally(async () => {
             await prisma.$disconnect();
         });
-}else{
+} else {
     console.log('\nSeeding is disabled. Set the "seed" environment variable to "true" to enable.')
 }
