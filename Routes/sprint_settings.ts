bf0510ff1f509237cf8aea81e085c2d46ea62d3b
gifederspiel <gian.fedrespiel@bluewin.ch>
2022-02-15 11:53:15 +0100

    PPK-25 Konfiguration Board doone

--- Routes/sprint_settings.ts
+++ Routes/sprint_settings.ts
@@ -1,15 +1,43 @@
-import express from 'express';
+import express, { query } from 'express';
 import path from 'path'
-import ejs from 'ejs';
+
+import { PrismaClient } from '@prisma/client'
+import { parse } from 'path/posix';
+const prisma = new PrismaClient()
 
 let router = express.Router();
+var currentTime = new Date();
 
 router.get('/:year/:pi', async (req, res) => {
     if (req.params.pi?.match(/^.*.js|.*.css$/)) {
         res.sendFile(path.join(__dirname, '../Public', req.params.pi));
     }
     else{
-        res.render("sprint_verwaltung", {query : req.query, params : req.params});
+        if( parseInt(req.params.year) >= 2020 && 
+            parseInt(req.params.year) <= 2100 &&(
+            req.params.pi == "PI-01" ||
+            req.params.pi == "PI-02" ||
+            req.params.pi == "PI-03" ||
+            req.params.pi == "PI-04")
+          ){
+            res.render("sprint_verwaltung", {params : req.params});
+        }
+        else{
+            res.redirect("/sprint_verwaltung/" + currentTime.getFullYear() + "/PI-01");
+            console.log("Page not Found");
+        }        
     }    
 });
+// router.post('/:year/:pi', async (req, res) => {
+//    const pi = prisma.pi.findFirst({where: {
+//        year: req.params.year,
+//        AND:{
+//            iteration: req.params.pi
+//        }
+//    }})
+
+
+//    res.render("sprint_verwaltung", {params : req.params});
+//});
+
 export = router;
