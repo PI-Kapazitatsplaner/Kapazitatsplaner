d8ea9db680394dbf40729b509452d638fbf554d8
roehh <git@roeh.ch>
2022-02-21 15:52:27 +0100

    fixed code scaning errors

---
+++ src/middleware/fileSender/fileSender.ts
@@ -0,0 +1,13 @@
+import path from 'path';
+import fs from 'fs';
+import express from 'express'
+
+export default function sendFileIfParamEqualsName(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const files = fs.readdirSync(path.join(__dirname, '../Public'));
+    const file = files.find(f => f.toLowerCase() === req.params.year?.toLowerCase()) || files.find(f => f.toLowerCase() === req.params.month?.toLowerCase());
+    if (file) {
+        res.sendFile(path.join(__dirname, '../Public', file));
+    } else {
+        next();
+    }
+};
