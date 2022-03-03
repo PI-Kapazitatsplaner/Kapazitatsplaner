229f164f1edf16d4bdb3b4aa6fd9e2fc2270dfe9
gifederspiel <gian.fedrespiel@bluewin.ch>
2022-03-03 13:21:03 +0100

    year changing possible

--- Routes/sprint_settings.ts
+++ Routes/sprint_settings.ts
@@ -30,23 +30,20 @@ router.get("/:year/:pi", async (req, res) => {
           },
         },
       });
-      if (pi !== null && pi.planungVon !== null) {
+      let sprintsInPi;
+      if(pi !== null){
         //Sprints Finden
-        const sprintsInPi = await prisma.sprint.findMany({
+          sprintsInPi = await prisma.sprint.findMany({
           where: {
-            piId: Number(pi.iteration.charAt(4)),
+            piId: pi.id,
           },
         });
-        res.render("sprint_verwaltung", {
-          params: req.params,
-          sprints: sprintsInPi,
-          pi: pi,
-        });
-      } else {
-        res.redirect(
-          "/sprint_verwaltung/" + currentTime.getFullYear() + "/PI-01"
-        );
       }
+      res.render("sprint_verwaltung", {
+        params: req.params,
+        sprints: sprintsInPi,
+        pi: pi,
+      });
     } else {
       res.redirect(
         "/sprint_verwaltung/" + currentTime.getFullYear() + "/PI-01"
@@ -55,47 +52,65 @@ router.get("/:year/:pi", async (req, res) => {
   }
 });
 //Update
-router.post("/:year/:pi", async (req, res) => {
-  //Planungstage Updaten
-  const updatePlanningDates = await prisma.pi.update({
-    where: {
-      piKey: {
-        year: req.params.year,
-        iteration: req.params.pi,
-      },
-    },
-    data: {
-      planungVon: req.body.planungStart + "T00:00:00.000Z",
-      planungBis: req.body.planungEnde + "T00:00:00.000Z",
-    },
-  });
-  //Pi Finden
-  const findCurrentPi = await prisma.pi.findUnique({
-    where: {
-      piKey: {
-        year: req.params.year,
-        iteration: req.params.pi,
-      },
-    },
-  });
-  //Einzelne Sprints updaten
-  for(let i = 1; i <= 6; i++){
-    if (findCurrentPi !== null) {
-      const updateSprint = await prisma.sprint.update({
+router.post("/:year/:pi", async (req, res) => {  
+  if(req.body.planungStart !== "" && req.body.planungEnde !== ""){
+    if (
+      parseInt(req.params.year) >= 2020 &&
+      parseInt(req.params.year) <= 2100 &&
+      (req.params.pi == "PI-01" ||
+        req.params.pi == "PI-02" ||
+        req.params.pi == "PI-03" ||
+        req.params.pi == "PI-04")
+    ) {
+      //Planungstage Updaten
+      const updatedPlanningDates = await prisma.pi.upsert({
         where: {
-          sprintKey: {
-            piId: findCurrentPi.id,
-            sprintNumber: i,
+          piKey: {
+            year: req.params.year,
+            iteration: req.params.pi,
           },
         },
-        data: {
-          von: req.body.start[i-1] + "T00:00:00.000Z",
-          bis: req.body.ende[i-1] + "T00:00:00.000Z",
+        update: {
+          planungVon: req.body.planungStart + "T00:00:00.000Z",
+          planungBis: req.body.planungEnde + "T00:00:00.000Z",
+        },
+        create: {
+          year: req.params.year,
+          iteration: req.params.pi,
+          planungVon: req.body.planungStart + "T00:00:00.000Z",
+          planungBis: req.body.planungEnde + "T00:00:00.000Z",
         },
       });
+      //Einzelne Sprints updaten
+      for (let i = 1; i <= 6; i++) {
+        if (updatedPlanningDates !== null) {
+          const updateSprint = await prisma.sprint.upsert({
+            where: {
+              sprintKey: {
+                piId: updatedPlanningDates.id,
+                sprintNumber: i,
+              },
+            },
+            update: {
+              von: req.body.start[i - 1] + "T00:00:00.000Z",
+              bis: req.body.ende[i - 1] + "T00:00:00.000Z",
+            },
+            create: {
+              piId: updatedPlanningDates.id,
+              sprintNumber: i,
+              von: req.body.start[i - 1] + "T00:00:00.000Z",
+              bis: req.body.ende[i - 1] + "T00:00:00.000Z",
+            },
+          });
+        }
+      }
+      res.redirect("/sprint_verwaltung/" + req.params.year + "/" + req.params.pi);
+    } else {
+      console.log("Params not valid");
     }
+  }else{
+    res.status(400);
   }
-  res.redirect("/sprint_verwaltung/" + req.params.year + "/" + req.params.pi);
 });
 
 export = router;
