36772bd94b2a36b19939e33512bd2ed6d4112c2f
gifederspiel <gian.fedrespiel@bluewin.ch>
2022-03-22 10:41:32 +0100

    added team calendar

--- src/Routes/team.ts
+++ src/Routes/team.ts
@@ -1,11 +1,31 @@
-import express from 'express';
+import express from "express";
+import prisma from "../prisma/client";
+import sendFileIfParamEqualsName from '../middleware/fileSender/fileSender';
 
 let router = express.Router();
+var currentTime = new Date();
 
-router.get('/', async (req, res) => {
-    const header = { currSite: 1, username: req.user.name };
-    res.render("team_kalender", { header, prefersWhiteMode: req.user.prefersWhiteMode });
+
+router.get("/:year/:pi", sendFileIfParamEqualsName, async (req, res) => {
+  const header = { currSite: 1, username: req.user.name };
+  if (
+    parseInt(req.params.year) >= 2020 &&
+    parseInt(req.params.year) <= 2100 &&
+    (req.params.pi == "PI-01" ||
+      req.params.pi == "PI-02" ||
+      req.params.pi == "PI-03" ||
+      req.params.pi == "PI-04")
+  ) {
+    res.render("team_kalender", {
+      header,
+      prefersWhiteMode: req.user.prefersWhiteMode,
+      params: req.params,
+    });
+  }else {
+    res.redirect(
+      "/team_kalender/" + currentTime.getFullYear() + "/PI-01"
+    );
+  }
 });
-
 
 export = router;
--- src/middleware/fileSender/fileSender.ts
+++ src/middleware/fileSender/fileSender.ts
@@ -4,7 +4,9 @@ import express from 'express'
 
 export default function sendFileIfParamEqualsName(req: express.Request, res: express.Response, next: express.NextFunction) {
     const files = fs.readdirSync(path.join(__dirname, '../../Public'));
-    const file = files.find(f => f.toLowerCase() === req.params.year?.toLowerCase()) || files.find(f => f.toLowerCase() === req.params.month?.toLowerCase());
+    const file =    files.find(f => f.toLowerCase() === req.params.year?.toLowerCase()) || 
+                    files.find(f => f.toLowerCase() === req.params.month?.toLowerCase()) || 
+                    files.find(f => f.toLowerCase() === req.params.pi?.toLowerCase());
     if (file) {
         res.sendFile(path.join(__dirname, '../../Public', file));
     } else {
